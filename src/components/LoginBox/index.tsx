cf4003676a896b0d590b0c84e1b44ef86952b0d3
Luciano Amâncio <madruga665@hotmail.com>
2021-10-21 20:05:44 -0300

    add react-icons

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,9 +1,12 @@
 import styles from './styles.module.scss'
+import { VscGithubInverted } from 'react-icons/vsc'
+
 export function LoginBox() {
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
       <a href="#" className={styles.signInWithGithub}>
+        <VscGithubInverted size='24' />
         entrar com github
       </a>
     </div>
