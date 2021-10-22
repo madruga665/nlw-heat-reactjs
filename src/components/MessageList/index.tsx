c22c32359d98185e32330675ac8198e05d105bcf
Luciano Amâncio <madruga665@hotmail.com>
2021-10-21 21:59:02 -0300

    add componente MessageList

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,22 @@
+import styles from './styles.module.scss'
+import logoImg from '../../assets/logo.svg'
+
 export function MessageList() {
   return (
-    <div>
-      <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Aqui vai o texto da mensagem</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/madruga665.png" alt="madruga" />
+            </div>
+            <span>Luciano Amâncio</span>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
